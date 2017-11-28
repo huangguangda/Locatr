21ea8394a6a05c9b0d17b3d84800c67d5eb23934
huangguangda <2397923107@qq.com>
2017-11-28 23:03:20 +0800

    20171128.111

---
+++ app/src/main/java/cn/edu/gdmec/android/locatr/LocatrFragment.java
@@ -0,0 +1,25 @@
+package cn.edu.gdmec.android.locatr;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+/**
+ * Created by Jack on 2017/11/28.
+ */
+
+public class LocatrFragment extends Fragment{
+    private ImageView mImageView;
+    public static LocatrFragment newInstance(){
+        return new LocatrFragment ();
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View v = inflater.inflate ( R.layout.fragment_locatr, container, false );
+        mImageView = (ImageView) v.findViewById ( R.id.image );
+        return v;
+    }
+}
